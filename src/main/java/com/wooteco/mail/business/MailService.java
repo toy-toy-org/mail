07c1487d1b2313ddf955b48688f12f39756d0f02
김성산 <kimseongsan@gimseongsan-ui-MacBookAir.local>
2023-06-19 23:11:21 +0900

    feat: 카프카 컨슈밍 로직 추가

--- src/main/java/com/wooteco/mail/business/MailService.java
+++ src/main/java/com/wooteco/mail/business/MailService.java
@@ -2,6 +2,7 @@ package com.wooteco.mail.business;
 
 import com.wooteco.mail.dto.MailSendRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,16 @@ public class MailService {
 
     public void send(MailSendRequest mailSendRequest) {
         mailSender.send(mailSendRequest.toMailMessage());
+    }
+
+    @KafkaListener(
+            id="mail-send",
+            topics="sign-up",
+            clientIdPrefix = "clientId",
+            properties = {"enable.auto.commit:false", "auto.offset.reset:latest"}
+    )
+    public void listen(String email) {
+        MailSendRequest request = new MailSendRequest(email, "HI", "BYE!");
+        mailSender.send(request.toMailMessage());
     }
 }
